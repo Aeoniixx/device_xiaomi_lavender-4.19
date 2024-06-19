cfb449e49d3ca06f37dbf368366c19502d6969ef
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    lavender: Build AOSP Radio HIDL->AIDL Wrapper
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- device.mk
+++ device.mk
@@ -421,6 +421,7 @@ PRODUCT_PACKAGES += \
     rild
 
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2 \
