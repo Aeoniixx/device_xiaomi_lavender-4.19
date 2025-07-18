00f674dc0ba052565faecca1ce316cbb867ede1c
pix106 <sbordenave@gmail.com>
2025-07-18 21:33:38 +0530

    lavender: overlay: Switch to RRO from Overlays
    * Cleanup and update overlay configs while at it
    
    Co-authored-by: Aeoniixx <aeoniixx@gmail.com>
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- device.mk
+++ device.mk
@@ -321,14 +321,17 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_PACKAGES += \
-    SettingsOverlay \
-    NoCutoutOverlay \
-    DeviceAsWebcamLavender
+    LavenderApertureOverlay \
+    LavenderCarrierConfigOverlay \
+    LavenderDeviceAsWebcam \
+    LavenderFrameworksOverlay \
+    LavenderSettingsOverlay \
+    LavenderTelephonyOverlay \
+    LavenderWifiOverlay 
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
